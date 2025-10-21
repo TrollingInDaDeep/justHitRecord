a7ebe5dbffbc0a0c070fc79cc5f25a10480eaaed
TrollingInDaDeep <dorieder@gmx.ch>
2025-10-21 23:15:52 +0200

    led on off on button press working

--- arduino/esp32_recorder/esp32_recorder.ino
+++ arduino/esp32_recorder/esp32_recorder.ino
@@ -1,14 +1,33 @@
 // ESP32 Code to record audio from jack socket and save to SD Card
 
-
-
+///
+/// variables
+///
+int ledPin = 18;
+int btnPin = 19;
+bool isRecording = false; //if recording is armed
+bool stateChanged = false; //if the button state already changed
+int debounce = 10; //button debounce time in ms
 
 void setup() {
-  
-
+  pinMode(ledPin, OUTPUT);
+  pinMode(btnPin, INPUT_PULLUP);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (digitalRead(btnPin)) {
+    if (!stateChanged) {
+      isRecording = !isRecording;
+      digitalWrite(ledPin, isRecording);
+      Serial.println(isRecording);
+      stateChanged = true;
+    }
+    
+  }
+  if (!digitalRead(btnPin)) {
+    stateChanged = false;
+  }
+  delay(debounce);
 
 }
