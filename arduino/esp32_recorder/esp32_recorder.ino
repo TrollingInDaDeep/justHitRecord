e711ee5afac274b46f74169d541d8a04745db102
TrollingInDaDeep <dorieder@gmx.ch>
2026-01-09 22:04:45 +0100

    can now read the files. just need to import them in audacity. basically done. next step, save recordings separately in folders and individual files

--- arduino/esp32_recorder/esp32_recorder.ino
+++ arduino/esp32_recorder/esp32_recorder.ino
@@ -3,6 +3,8 @@
 #include "SPI.h"
 #include "AudioTools.h"
 #include "AudioTools/AudioLibs/AudioBoardStream.h"
+#include <WiFi.h>
+#include <credentials.h>
 // ESP32 Code to record audio from jack socket and save to SD Card
 
 //ideas
@@ -43,6 +45,13 @@ float currentMillis; //current ms timestamp
 float btnPressMillis; //ms Timestamp when btn was pressed
 int debounceMillis = 50; //button debounce time in ms
 
+
+//WIFI
+const char *ssid = SECRET_ssid;
+const char *password = SECRET_password;
+NetworkServer server(80);
+
+
 //Audio I2s PCM1808 stuff
 AudioInfo info(44100, 2, 32); //samplerate
 I2SStream i2sStream;
@@ -55,7 +64,7 @@ int i2s_data = 17; //TX2 (Pin 17) Data (out)
 int i2s_mck = 3; //RX0 (Pin 3) MCLK (SCK)
 
 //SDCard Stuff
-const char *file_name = "/aufnahme_.wav";
+const char *file_name = "/aufnahme_.raw";
 File recFile; //Stream that will be saved to SD Card
 uint64_t end_time; //trigger to call endRecord
 
@@ -193,20 +202,77 @@ void readBtn() {
       lastBtnState = btnState;
     }
   }
-  
+}
 
-  // if (millis() - btnPressMillis > debounceMillis) {
-  //   if (btnState && !lastBtnState) {
-  //     Serial.println("Press");
-  //     stateChanged = true;
-  //     lastBtnState = btnState;
-  //   }
+void startNetwork(){
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
-  //   if (!btnState && lastBtnState) {
-  //     Serial.println("Release");
-  //     lastBtnState = btnState;
-  //   }
-  // }
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
+}
+
+void loopNetwork(){
+  NetworkClient client = server.accept();  // listen for incoming clients
+
+  if (client) {                     // if you get a client,
+    Serial.println("New Client.");  // print a message out the serial port
+    String currentLine = "";        // make a String to hold incoming data from the client
+    while (client.connected()) {    // loop while the client's connected
+      if (client.available()) {     // if there's bytes to read from the client,
+        char c = client.read();     // read a byte, then
+        Serial.write(c);            // print it out the serial monitor
+        if (c == '\n') {            // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/H\">here</a> to turn the LED on pin 5 on.<br>");
+            client.print("Click <a href=\"/L\">here</a> to turn the LED on pin 5 off.<br>");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {  // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(ledPin, HIGH);  // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(ledPin, LOW);  // GET /L turns the LED off
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("Client Disconnected.");
+  }
 }
 
 void setup() {
@@ -239,6 +305,7 @@ void setup() {
 
   error_blink(69); //show success message!!
   lastBtnState = digitalRead(recBtnPin);
+  //startNetwork();
 }
 
 void loop() {
@@ -266,5 +333,7 @@ void loop() {
     
     stateChanged = false;
   }
-
+  
+  
+  //loopNetwork();
 }
